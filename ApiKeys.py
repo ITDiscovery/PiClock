17e3aa6c2bcfebd637f1472c96e64c3e093b1c18
ITDiscovery <raspian@itdiscovery.info>
2020-01-22 19:01:05 -0600

    First commit

---
+++ ApiKeys.py
@@ -0,0 +1,9 @@
+# change this to your API keys
+# DarkSky API key
+dsapi = '1d2469965b2487b54b6d6cc43f2d368b'
+
+# Map API keys -- only needs 1 of the following
+# Google Maps API key (if usemapbox is not set in Config)
+googleapi = 'AIzaSyDLSRAHbL6jYQSvlBarUqB0dtZPemBY43M'
+# Mapbox API key (access_token) [if usemapbox is set in Config]
+#mbapi = 'YOUR MAPBOX ACCESS TOKEN'
